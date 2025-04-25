9f83c2169a9e0b161896ee5bea353808766693c3
ahsalmonskinroll <kumsal.celebiii@gmail.com>
2025-04-26 02:53:21 +0300

    güncelleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import dalosimg from "../src/design/assets/hero/dalosnetwork.svg";
 import daloslogo from "../src/design/assets/hero/logo.svg";
-import heroMountainimg from "../src/design/assets/hero/herobgGroup.png";
-import heroCloudimg from "../src/design/assets/hero/heroCloud.png";
+import cryptoimg from "../src/design/assets/aboutUs/Cryptocurrency.svg";
+import securityimg from "../src/design/assets/aboutUs/security.svg";
+import wingsimg from "../src/design/assets/aboutUs/wings.svg";
 
 function App() {
   return (
@@ -32,14 +33,72 @@ function App() {
               </div>
             </header>
             <section className="hero-section df jc whitef robotof brownf f32">
-              <div>
-                Unlocking Tomorrow, Today - Where Blockchain Meets <br />
-                <span className="df jc">Infinite Possibility</span>
+              <div className="row">
+                <div className="col">
+                  Unlocking Tomorrow, Today - Where Blockchain Meets <br />
+                  <span className="df jc">Infinite Possibility</span>
+                </div>
+                <div className="col-12">
+                  <button className="btn1 f24 fw500 brownf ">About Us</button>
+                </div>
               </div>
             </section>
           </div>
         </div>
-        <div className="about-us">about us</div>
+        <div className="about-us">
+          <div className="row">
+            <div className="col-12 aboutUst"></div>
+            <div className="col-12 about aboutbg">
+              <div className="aboutsection playfair f57 whitef ">About Us</div>
+              <div className="row aboutlayout df aic ">
+                <div className="col-sm-12 col-md-12 col-lg-4 df jc">
+                  <div className="row">
+                    <div className="col-12 df jc">
+                      <img src={cryptoimg} alt="" />
+                    </div>
+                    <div className="col-12 df jc f18 blackf robotof">
+                      At Dalos Network, we are dedicated to revolutionizing
+                      industries through innovative blockchain solutions. With a
+                      focus on decentralization, security, and accessibility, we
+                      transcend boundaries across various blockchain networks.
+                      Our mission is to create a more equitable and accessible
+                      digital world for all.
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-12 col-lg-4 df jc">
+                  <div className="row">
+                    <div className="col-12 df jc">
+                      <img src={securityimg} alt="" />
+                    </div>
+                    <div className="col-12 df jc f18 blackf robotof">
+                      We prioritize security, privacy, and accessibility,
+                      ensuring our applications uphold the highest standards
+                      across multiple blockchain networks. Our goal is to
+                      leverage blockchain technology to its fullest potential,
+                      fostering inclusivity and empowering individuals globally.
+                    </div>
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-12 col-lg-4 df jc">
+                  <div className="row">
+                    <div className="col-12 df jc">
+                      <img src={wingsimg} alt="" />
+                    </div>
+                    <div className="col-12 df jc f18 blackf robotof">
+                      Join us at Dalos Network as we lead the charge in
+                      pioneering decentralized solutions. Together, we'll shape
+                      a future where blockchain transforms industries and
+                      creates opportunities for everyone.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 grad3"></div>
+          </div>
+        </div>
         <div className="services">services</div>
         <div className="contact">contact</div>
         <footer>footer</footer>
